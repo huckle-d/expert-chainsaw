8641932ab9116dd23536007e4520f3dd20683d7f
dhuc995 <dhuc995@aucklanduni.ac.nz>
2022-06-23 16:07:07 +1200

    added timer Added a timer to the second fragment which starts from zero and ticks up for as long as the fragment is active.

--- app/src/main/java/com/example/practicetracker/SecondFragment.java
+++ app/src/main/java/com/example/practicetracker/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -36,6 +37,11 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        // Get reference to the timer (chronometer)
+        Chronometer timer = (Chronometer) getView().findViewById(R.id.timer);
+        // Start the timer
+        timer.start();
     }
 
     @Override
